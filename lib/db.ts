d07b1b62747cea8cda0ccd8d5ffa25e0ea5cf991
seyaendris <seide9894@gmail.com>
2024-12-14 23:53:55 +0300

    created a database connection

---
+++ lib/db.ts
@@ -0,0 +1,14 @@
+
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI!);
+    console.log(`Successfully connected to mongoDB 🥂`);
+  } catch (error: any) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
